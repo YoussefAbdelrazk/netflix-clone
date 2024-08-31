9266b6de5c0a00bdf9555290c71c17dca0496795
= <youssefabdelrazk2002@gmail.com>
2024-08-31 06:24:19 +0300

    update

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -36,7 +36,7 @@ const saveShow = async() => {
     <div  className=" w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer p-2 relative">
               <img className="w-full h-auto block" src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item.title} />
               <div className=" absolute inset-0 hover:bg-black/80 opacity-0 hover:opacity-100 text-white duration-200 ">
-                <Link to={`/movie/${item.id}`} className="text-xs md:text-sm whitespace-normal flex justify-center items-center h-full" >{item?.title}</Link>
+                <Link to={`/movie/${item?.id}`} className="text-xs md:text-sm whitespace-normal flex justify-center items-center h-full" >{item?.title}</Link>
                 <p onClick={saveShow}>
                   {Like ? <FaHeart size={20} className=" absolute top-4 left-4 text-gray-300"/> : <FaRegHeart size={20} className=" absolute top-4 left-4 text-gray-300"/>} 
                  </p>
